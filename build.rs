792a1194408067183ee8a64e394a17937889865c
monzer <almonther@ecom.io>
2025-03-04 19:57:55 +0200

    finished UI impl

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let config =
-    slint_build::CompilerConfiguration::new()
-    .with_style("material".into());
+    // let config =
+    // slint_build::CompilerConfiguration::new()
+    // .with_style("material".into());
 
-    slint_build::compile_with_config("ui/main.slint", config).unwrap();
+    // slint_build::compile_with_config("ui/main.slint", config).unwrap();
+    slint_build::compile("ui/main.slint");
+    // slint_build::compile("ui/login.slint");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use slint::ComponentHandle;
+
 slint::include_modules!();
 
-fn main() {
-    Riham::new().unwrap().run().unwrap();
+fn main() -> Result<(), slint::PlatformError> {
+    let riham = Riham::new().unwrap();
+
+    let weak = riham.as_weak();
+    riham.on_login(move |username, password| {
+        let riham = weak.unwrap();
+        if username.as_str() == "admin" && password.as_str() == "admin" {
+            riham.set_currentPage(slint::SharedString::from("dashboard"));
+            return riham.set_loggedin(true);
+        }
+        riham.invoke_loginError();
+    });
+    riham.run()
 }
 
